e7e291f3fb1f79ef5d659c285459ea54e2966d8d
EC2 Default User <saki120428@gmail.com>
2024-03-07 01:40:32 +0000

    モーダル修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -8,14 +8,14 @@ class NotificationsController < ApplicationController
 
     # 確認済みの通知を取得
     @checked_notifications = @notices.where(is_checked: true).limit(20)
-    
+
     # 通知を確認済みに更新
     current_user.passive_notifications.update_all(is_checked: true)
-    render partial: "index"
+    # render partial: "index"
   end
 
   def update_checked
     current_user.passive_notifications.update_all(is_checked: true)
-    head :no_content
+
   end
 end
