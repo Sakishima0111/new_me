796cdf48dd137c9b291a452d8b8ec8a455796093
EC2 Default User <saki120428@gmail.com>
2024-03-21 14:16:32 +0000

    デプロイ直前

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,62 @@
-Admin.create!(
+Admin.find_or_create_by!(
    email: 'admin@admin',
    password: 'testtest'
 )
-
-User.create!(
+User.find_or_create_by!(
    email: 'cat@cat',
    nickname: 'ネコ',
    introduction: 'テストユーザー1',
    password: "testtest"
 )
-User.create!(
+User.find_or_create_by!(
    email: 'dog@dog',
    nickname: 'いぬ',
    introduction: 'テストユーザー2',
    password: "testtest"
 )
-
-Category.create!([
+User.find_or_create_by!(
+   email: 'alpaca@hoge',
+   nickname: 'アルパカ',
+   introduction: 'テストユーザー3',
+   password: "testtest"
+)
+User.find_or_create_by!(
+   email: 'bear@hoge',
+   nickname: 'クマ',
+   introduction: 'テストユーザー4',
+   password: "testtest"
+)
+User.find_or_create_by!(
+   email: 'camel@hoge',
+   nickname: 'ラクダ',
+   introduction: 'テストユーザー5',
+   password: "testtest"
+)
+User.find_or_create_by!(
+   email: 'deer@hoge',
+   nickname: 'シカ',
+   introduction: 'テストユーザー6',
+   password: "testtest"
+)
+User.find_or_create_by!(
+   email: 'donkey@hoge',
+   nickname: 'ロバ',
+   introduction: 'テストユーザー7',
+   password: "testtest"
+)
+User.find_or_create_by!(
+   email: 'fox@hoge',
+   nickname: 'キツネ',
+   introduction: 'テストユーザー8',
+   password: "testtest"
+)
+User.find_or_create_by!(
+   email: 'goat@hoge',
+   nickname: 'ヤギ',
+   introduction: 'テストユーザー9',
+   password: "testtest"
+)
+Category.find_or_create_by!([
   { name: '勉強・資格' },
   { name: '筋トレ・ダイエット' },
   { name: '生活' },
@@ -33,4 +73,7 @@ Category.create!([
   { name: '環境' },
   { name: 'その他' },
 ])
+
+
+
 
